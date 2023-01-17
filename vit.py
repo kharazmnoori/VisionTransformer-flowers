280289f7756be77468dc5c44e6de2d50d74f0444
Kharazm Noori <kharazm.nori@gmail.com>
2023-01-17 18:21:24 +0100

    configuration

--- vit.py
+++ vit.py
@@ -5,5 +5,24 @@
  ***
 """
 
-
 import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
+
+ 
+# configuration
+if __name__ == '__main__':
+    config = {}
+    config["num_layers"] = 12
+    config["hidden_layers"] = 768
+    config["num_heads"] = 12
+    config["num_dim"] = 3072
+    config["dropout_rate"] = 0.1
+    config["num_patches"] = 256
+    config["patch_size"] = 32
+    config["num_classes"] = 3
+
+
